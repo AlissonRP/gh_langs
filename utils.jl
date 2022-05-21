ce41e98f75abba9952702bb86288173ed4069b66
AlissonRP <alirpereira887@gmail.com>
2022-05-21 15:37:40 -0300

    fav_lang

--- utils.jl
+++ utils.jl
@@ -8,8 +8,15 @@ pr = DataFrame(CSV.File("data/prs.csv"))
 issues = DataFrame(CSV.File("data/issues.csv"))
 total = DataFrame(CSV.File("data/repos.csv"))
 
-top_5 = function (df,variable)
-    @pipe df |> 
-    groupby(_, variable) |> 
-    combine(_, :count => mean => :media)
+top_5 = function (df::DataFrame, variable::Symbol)
+    @pipe df |>
+          groupby(_, variable) |>
+          combine(_, :count => mean => :media)
+end
+
+fav_langs = function (df::DataFrame, langs::Vector)
+    @pipe df[findall(in(langs), df[!, :name]), :] |>
+          groupby(_, [:year, :name]) |>
+          combine(_, :count => (mean => :media))
+
 end
