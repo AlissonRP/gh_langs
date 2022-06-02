8de1290ec347d986c49e37c9ab069ac8ddcea351
AlissonRP <alirpereira887@gmail.com>
2022-06-02 15:07:02 -0300

    add: slides

---
+++ docs/utils.jl
@@ -0,0 +1,25 @@
+using DataFrames
+import CSV
+using Plots
+default(formatter=identity)
+using Statistics
+using Pipe: @pipe
+pr = DataFrame(CSV.File("data/prs.csv"))
+issues = DataFrame(CSV.File("data/issues.csv"))
+total = DataFrame(CSV.File("data/repos.csv"))
+
+top_5 = function (df::DataFrame, variable::Symbol)
+    @pipe df |>
+          groupby(_, variable) |>
+          combine(_, :count => mean => :media)
+end
+
+fav_langs = function (df::DataFrame, langs::Vector)
+    @pipe df[findall(in(langs), df[!, :name]), :] |>
+          groupby(_, [:year, :name]) |>
+          combine(_, :count => (mean => :media))
+
+end
+
+lag(x) = [x[i] - x[i-1] for i in 2:length(x)]
+
