f9a48169f29c118462ec70c35c7eb344017fa7df
AlissonRP <alirpereira887@gmail.com>
2022-06-03 02:51:57 -0300

    0.8 version

--- utils.jl
+++ utils.jl
@@ -4,9 +4,6 @@ using Plots
 default(formatter=identity)
 using Statistics
 using Pipe: @pipe
-pr = DataFrame(CSV.File("data/prs.csv"))
-issues = DataFrame(CSV.File("data/issues.csv"))
-total = DataFrame(CSV.File("data/repos.csv"))
 
 top_5 = function (df::DataFrame, variable::Symbol)
     @pipe df |>
