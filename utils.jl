857d59c4839712b53b5a944a99e686fc7746ff78
AlissonRP <alirpereira887@gmail.com>
2022-06-05 00:07:43 -0300

    horizontal plot

--- utils.jl
+++ utils.jl
@@ -1,7 +1,8 @@
 using DataFrames
 import CSV
 using Plots
-default(formatter=identity)
+default(formatter=identity, tickfontsize=7, titlefontsize=12,
+    legend=:topleft)
 using Statistics
 using Pipe: @pipe
 
@@ -20,3 +21,17 @@ end
 
 lag(x) = [x[i] - x[i-1] for i in 2:length(x)]
 
+top_5_count = function (df::DataFrame, var::Symbol)
+    @pipe df |>
+          groupby(_, var) |>
+          combine(_, :count => sum => :total) |>
+          sort(_, :total, rev=true) |>
+          first(_, 5)
+end
+
+barh = function (df::DataFrame, lc::String)
+    x = top_5_count(df, :name)[:, :total]
+    y = top_5_count(df, :name)[:, :name]
+    bar(x, orientation=:h, yticks=(1:5, y), legend=false, yflip=true, fill=["#f3ff33", "#337aff",
+            "#ce640c", "#b60000", lc], xticks=0:2*10^6:7.5*10^6)
+end
